8bfa8918b462f7bde87af711553a821d59abc87a
shanshi <yamilacariati@hotmail.com>
2017-07-04 19:27:15 -0300

    + Migration Pedido_FechaEntrega_Nullable.

---
+++ src/SIPI.Data.EF/Migrations/201707042225129_Pedido_FechaEntrega_Nullable.cs
@@ -0,0 +1,18 @@
+namespace SIPI.Data.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Pedido_FechaEntrega_Nullable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Pedido", "FechaEntregado", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Pedido", "FechaEntregado", c => c.DateTime(nullable: false));
+        }
+    }
+}

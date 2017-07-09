8fa7b5151184ef00e5d2be794235eb8a521fd7f9
shanshi <yamilacariati@hotmail.com>
2017-07-09 20:02:12 -0300

    * seed.sql: fix formato fechas, borrado de contenido de tablas y print para verificar borrado/inserción.

--- database/seed.sql
+++ database/seed.sql
@@ -1,11 +1,94 @@
+/* Borrado */
+
+PRINT 'BORRANDO CONTENIDO DE LAS TABLAS'
+
+PRINT ''
+PRINT 'Tabla: Orador'
+DELETE FROM Orador
+DBCC CHECKIDENT ('[Orador]', RESEED, 0);
+GO
+
+PRINT ''
+PRINT 'Tabla: InsumoMedioAudiovisual'
+DELETE FROM InsumoMedioAudiovisual
+-- no tiene identity
+
+PRINT ''
+PRINT 'Tabla: MedioAudiovisual'
+DELETE FROM MedioAudiovisual
+DBCC CHECKIDENT ('[MedioAudiovisual]', RESEED, 0);
+GO
+
+PRINT ''
+PRINT 'Tabla: Categoria'
+DELETE FROM Categoria
+-- no tiene identity
+
+PRINT ''
+PRINT 'Tabla: Tipo'
+DELETE FROM Tipo
+DBCC CHECKIDENT ('[Tipo]', RESEED, 0);
+GO
+
+PRINT ''
+PRINT 'Tabla: Insumo'
+DELETE FROM Insumo
+-- no tiene identity
+
+PRINT ''
+PRINT 'Tabla: Pedido'
+DELETE FROM Pedido
+DBCC CHECKIDENT ('[Pedido]', RESEED, 0);
+GO
+
+PRINT ''
+PRINT 'Tabla: OperadorRol'
+DELETE FROM OperadorRol
+-- no tiene identity
+
+PRINT ''
+PRINT 'Tabla: Rol'
+DELETE FROM Rol
+-- no tiene identity
+
+PRINT ''
+PRINT 'Tabla: Usuario'
+DELETE FROM Usuario
+DBCC CHECKIDENT ('[Usuario]', RESEED, 0);
+GO
+
+PRINT ''
+PRINT 'Tabla: Localidad'
+DELETE FROM Localidad
+-- no tiene identity
+
+PRINT ''
+PRINT 'Tabla: Provincia'
+DELETE FROM Provincia
+-- no tiene identity
+
+PRINT ''
+PRINT '------------------------------------------------------------------------'
+PRINT ''
+
+/* Inserciones */
+
+PRINT 'INSERTANDO CONTENIDO EN LAS TABLAS'
+
+PRINT ''
+PRINT 'Tabla: Provincia'
 INSERT INTO Provincia (Id, Nombre) VALUES (1, 'Buenos Aires')
 GO
 
+PRINT ''
+PRINT 'Tabla: Localidad'
 INSERT INTO Localidad (Id, Nombre, Provincia_Id) VALUES (1, 'Buenos Aires', 1)
 GO
 INSERT INTO Localidad (Id, Nombre, Provincia_Id) VALUES (2, 'La Plata', 1)
 GO
 
+PRINT ''
+PRINT 'Tabla: Usuario'
 SET IDENTITY_INSERT Usuario ON 
 GO
 INSERT INTO Usuario (Id, Email, Nombre, Apellido, Contrasena, ContrasenaNueva, Hash, Altura, Calle, Direccion, Telefono, Piso, Discriminator, Localidad_Id, Provincia_Id) VALUES (1, 'miembro@mail.com', 'Don', 'Miembro', '1234', NULL, NULL, 4792, 'Charcas', 'Direccion', '47760626', '1', 'Miembro', 1, 1)
@@ -19,6 +102,8 @@ GO
 SET IDENTITY_INSERT Usuario OFF
 GO
 
+PRINT ''
+PRINT 'Tabla: Rol'
 INSERT INTO Rol (Id, Nombre, Descripcion) VALUES (1, 'Contenido', 'Lorem ipsum dolo e amet')
 GO
 INSERT INTO Rol (Id, Nombre, Descripcion) VALUES (2, 'Packaging', 'Lorem ipsum dolo e amet')
@@ -26,6 +111,8 @@ GO
 INSERT INTO Rol (Id, Nombre, Descripcion) VALUES (3, 'Vendedor', 'Lorem ipsum dolo e amet')
 GO
 
+PRINT ''
+PRINT 'Tabla: OperadorRol'
 INSERT INTO OperadorRol (Operador_Id, Rol_Id) VALUES (2, 1)
 GO
 INSERT INTO OperadorRol (Operador_Id, Rol_Id) VALUES (3, 2)
@@ -33,17 +120,21 @@ GO
 INSERT INTO OperadorRol (Operador_Id, Rol_Id) VALUES (4, 3)
 GO
 
+PRINT ''
+PRINT 'Tabla: Pedido'
 SET IDENTITY_INSERT Pedido ON
 GO
-INSERT INTO Pedido (Numero, CantidadPedido, Estado, Fecha, PrecioTotal, FechaEntregado, Miembro_Id) VALUES (1, 3, 1, '2017-07-07', 10.5, null, 1)
+INSERT INTO Pedido (Numero, CantidadPedido, Estado, Fecha, PrecioTotal, FechaEntregado, Miembro_Id) VALUES (1, 3, 1, '20170707', 10.5, null, 1)
 GO
-INSERT INTO Pedido (Numero, CantidadPedido, Estado, Fecha, PrecioTotal, FechaEntregado, Miembro_Id) VALUES (2, 3, 1, '2017-07-07', 150.5, null, 1)
+INSERT INTO Pedido (Numero, CantidadPedido, Estado, Fecha, PrecioTotal, FechaEntregado, Miembro_Id) VALUES (2, 3, 1, '20170707', 150.5, null, 1)
 GO
-INSERT INTO Pedido (Numero, CantidadPedido, Estado, Fecha, PrecioTotal, FechaEntregado, Miembro_Id) VALUES (3, 1, 1, '2017-07-08', 210.5, null, 1)
+INSERT INTO Pedido (Numero, CantidadPedido, Estado, Fecha, PrecioTotal, FechaEntregado, Miembro_Id) VALUES (3, 1, 1, '20170708', 210.5, null, 1)
 GO
 SET IDENTITY_INSERT Pedido OFF
 GO
 
+PRINT ''
+PRINT 'Tabla: Insumo'
 INSERT INTO Insumo (PedidoNumero, Numero, Precio, Tamano) VALUES (1, 1, 10.5, 80)
 GO
 INSERT INTO Insumo (PedidoNumero, Numero, Precio, Tamano) VALUES (2, 2, 100.5, 870)
@@ -57,29 +148,37 @@ GO
 INSERT INTO Insumo (PedidoNumero, Numero, Precio, Tamano) VALUES (3, 6, 150, 970)
 GO
 
+PRINT ''
+PRINT 'Tabla: Tipo'
 INSERT INTO Tipo (Nombre) VALUES ('Audio')
 GO
 INSERT INTO Tipo (Nombre) VALUES ('Video')
 GO
 
-INSERT INTO Categoria (Id, Nombre, FechaCreacion, Operador_Id) VALUES (1, 'Matrimonio', '2017-05-01', 2);
+PRINT ''
+PRINT 'Tabla: Categoria'
+INSERT INTO Categoria (Id, Nombre, FechaCreacion, Operador_Id) VALUES (1, 'Matrimonio', '20170501', 2);
 GO
-INSERT INTO Categoria (Id, Nombre, FechaCreacion, Operador_Id) VALUES (2, 'Juventud', '2017-05-01', 2);
+INSERT INTO Categoria (Id, Nombre, FechaCreacion, Operador_Id) VALUES (2, 'Juventud', '20170501', 2);
 GO
-INSERT INTO Categoria (Id, Nombre, FechaCreacion, Operador_Id) VALUES (3, 'Actualidad', '2017-05-01', 2);
-GO
-
-INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('2016-05-06', 'Consejos de familia', 'https://www.youtube.com/watch?v=ar17oemc-kI', 100, 1, 2)
-GO
-INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('2015-01-19', 'Adolescencia', 'https://www.youtube.com/watch?v=0f2XXh8XiTk', 70, 2, 2)
-GO
-INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('2014-07-22', 'Caminos inciertos', 'https://www.youtube.com/watch?v=2GBpF9R79H4', 80, 3, 2)
-GO
-INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('2017-03-22', 'Dinero y felicidad', 'https://www.youtube.com/watch?v=SOo7pBkWFEw', 800, 3, 2)
-GO
-INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('2014-07-22', 'Fidelidad', 'https://www.youtube.com/watch?v=WrKsS4KbGRE', 20, 1, 2)
+INSERT INTO Categoria (Id, Nombre, FechaCreacion, Operador_Id) VALUES (3, 'Actualidad', '20170501', 2);
 GO
 
+PRINT ''
+PRINT 'Tabla: MedioAudiovisual'
+INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('20160506', 'Consejos de familia', 'https://www.youtube.com/watch?v=ar17oemc-kI', 100, 1, 2)
+GO
+INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('20150119', 'Adolescencia', 'https://www.youtube.com/watch?v=0f2XXh8XiTk', 70, 2, 2)
+GO
+INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('20140722', 'Caminos inciertos', 'https://www.youtube.com/watch?v=2GBpF9R79H4', 80, 3, 2)
+GO
+INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('20170322', 'Dinero y felicidad', 'https://www.youtube.com/watch?v=SOo7pBkWFEw', 800, 3, 2)
+GO
+INSERT INTO MedioAudiovisual (FechaGrabacion, Tema, Url, Tamano, Categoria_Id, Tipo_Id) VALUES ('20140722', 'Fidelidad', 'https://www.youtube.com/watch?v=WrKsS4KbGRE', 20, 1, 2)
+GO
+
+PRINT ''
+PRINT 'Tabla: InsumoMedioAudiovisual'
 INSERT INTO InsumoMedioAudiovisual VALUES (1, 1, 3)
 GO
 INSERT INTO InsumoMedioAudiovisual VALUES (2, 2, 2)
@@ -103,13 +202,31 @@ GO
 INSERT INTO InsumoMedioAudiovisual VALUES (3, 6, 4)
 GO
 
-INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('Hernán', 'Rodriguez', '2014-01-05', 2, 1)
+PRINT ''
+PRINT 'Tabla: Orador'
+INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('Hernán', 'Rodriguez', '20140105', 2, 1)
 GO
-INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('Mariela', 'Lentini', '2014-01-05', 2, 2)
+INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('Mariela', 'Lentini', '20140105', 2, 2)
 GO
-INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('Paola', 'Casas', '2014-01-05', 2, 3)
+INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('Paola', 'Casas', '20140105', 2, 3)
 GO
-INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('José',  'King', '2014-01-05', 2, 4)
+INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('José',  'King', '20140105', 2, 4)
 GO
-INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('Carla', 'Mayada', '2014-01-05', 2, 5)
+INSERT INTO Orador (Nombre, Apellido, FechaCreacion, Operador_Id, MedioAudiovisual_Id) VALUES ('Carla', 'Mayada', '20140105', 2, 5)
 GO
+
+
+/* Verficación */
+
+--SELECT * FROM Provincia
+--SELECT * FROM Localidad
+--SELECT * FROM Usuario
+--SELECT * FROM Rol
+--SELECT * FROM OperadorRol
+--SELECT * FROM Pedido
+--SELECT * FROM Insumo
+--SELECT * FROM Tipo
+--SELECT * FROM Categoria
+--SELECT * FROM MedioAudiovisual
+--SELECT * FROM InsumoMedioAudiovisual
+--SELECT * FROM Orador

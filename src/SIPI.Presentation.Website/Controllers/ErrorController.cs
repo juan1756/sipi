19a5c63dc41ae1b50056f9a5f71042b5ebe97f54
Rodrigo Sevil <rodrigo.sevil@gmail.com>
2017-07-18 11:09:44 -0300

    Error handling

---
+++ src/SIPI.Presentation.Website/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace SIPI.Presentation.Website.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        [ActionName("404")]
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        [ActionName("500")]
+        public ActionResult Conflict()
+        {
+            return View();
+        }
+
+        [ActionName("501")]
+        public ActionResult NotImplemented()
+        {
+            return View();
+        }
+    }
+}
--- src/SIPI.Presentation.Website/Filters/Mvc/ExceptionHandlingAttribute.cs
+++ src/SIPI.Presentation.Website/Filters/Mvc/ExceptionHandlingAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Web.Mvc;
 
 namespace SIPI.Presentation.Website.Filters.Mvc
@@ -12,19 +13,9 @@ namespace SIPI.Presentation.Website.Filters.Mvc
             var ex = filterContext.Exception;
 
             if (ex is NotImplementedException)
-            {
-                filterContext.ExceptionHandled = true;
-                filterContext.HttpContext.Response.Clear();
-                filterContext.HttpContext.Response.TrySkipIisCustomErrors = true;
-                filterContext.HttpContext.Response.StatusCode = (int)System.Net.HttpStatusCode.ServiceUnavailable;
-                filterContext.Result = new ViewResult()
-                {
-                    ViewName = "503",
-                    ViewData = filterContext.Controller.ViewData,
-                    TempData = filterContext.Controller.TempData,
-                    MasterName = "_Layout"
-                };
-            }
+                throw new HttpException((int)System.Net.HttpStatusCode.NotImplemented, "Not Implemented");
+
+            throw new HttpException((int)System.Net.HttpStatusCode.InternalServerError, "InternalServerError");
         }
     }
 }

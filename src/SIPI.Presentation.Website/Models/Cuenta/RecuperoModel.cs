0c5da1d9be20de8ef8def3b0430e4dbb92258386
shanshi <yamilacariati@hotmail.com>
2017-07-23 15:56:47 -0300

    * Fix: mensaje del RecuperoModel para que sea coherente con los del login.

--- src/SIPI.Presentation.Website/Models/Cuenta/RecuperoModel.cs
+++ src/SIPI.Presentation.Website/Models/Cuenta/RecuperoModel.cs
@@ -9,7 +9,7 @@ namespace SIPI.Presentation.Website.Models.Cuenta
     public class RecuperoModel : LoginModel
     {
         [Display(Name = "Confirmación de contraseña")]
-        [Required(ErrorMessage = "El campo Confirmar contraseña es requerido")]
+        [Required(ErrorMessage = "El campo Confirmar contraseña es obligatorio")]
         [MaxLength(150, ErrorMessage = "El campo Confirmar contraseña puede contener como máximo 150 caracteres" )]
         [Compare(nameof(Contrasena), ErrorMessage = "El campo Confirmar contraseña debe coincidir con el campo Contraseña")]
         public string ConfirmarContrasena { get; set; }
